5125444df50ba98fa24a171ef7589ce8e47bf8ce
nex964 <firebeast18@gmail.com>
2019-10-21 02:04:11 +0530

    Nice

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { MTicketComponent } from './home/m-ticket/m-ticket.component';
 
 
 const routes: Routes = [
-  {
-    path: '', component: HomeComponent
-  }
+  {path: '', component: HomeComponent},
+  {path: 'ticket', component: MTicketComponent}
 ];
 
 @NgModule({
